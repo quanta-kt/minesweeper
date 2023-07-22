8c8a40150a83c15313834015e6e62d31e7ce251c
Abhijeeet Sonar <abhijeet.nkt@gmail.com>
2023-07-22 16:00:56 +0530

    Enable hosting react app from backend

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,10 +2,30 @@ mod create_game;
 mod join_game;
 mod ws;
 
-use actix_web::web;
+use std::path::PathBuf;
+
+use actix_files::{Files, NamedFile};
+use actix_web::{web, HttpRequest};
+
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let static_dir: PathBuf = "./static/".parse().unwrap();
+    let filename: PathBuf = req.match_info().query("filename").parse().unwrap();
+
+    let path = static_dir.join(filename);
+
+    if path == static_dir {
+        Ok(NamedFile::open(static_dir.join("index.html"))?)
+    } else {
+        Ok(NamedFile::open(static_dir.join(path))?)
+    }
+}
 
 pub fn routes() -> actix_web::Scope {
-    web::scope("/api")
+    let api_service = web::scope("/api")
         .service(create_game::create_game)
-        .service(join_game::join_game)
+        .service(join_game::join_game);
+
+    web::scope("")
+        .service(api_service)
+        .service(Files::new("/", "./static/").index_file("index.html"))
 }
